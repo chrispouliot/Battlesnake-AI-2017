059665a752d0717053203fb68d125a6cbcb50050
Christophe Pouliot <cristophepoug@gmail.com>
2017-03-03 14:30:14 -0800

    add logging

--- app.py
+++ app.py
@@ -19,4 +19,13 @@ def handle_move():
 
 
 if __name__ == 'main':
+    formatter = logging.Formatter("{%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.ERROR)
+    handler.setFormatter(formatter)
+
+    app.logger.addHandler(handler)
+
+    logging.getLogger('flask_cors').level = logging.INFO
+
     app.run()
