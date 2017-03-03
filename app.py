785636471f6d976e97c646568ef873543af8ac1c
Christophe Pouliot <cristophepoug@gmail.com>
2017-03-03 14:41:29 -0800

    Working server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask
+from flask import Flask, json, request
 app = Flask(__name__)
 
 
@@ -8,24 +8,15 @@ def handle_index():
     return "This isn't what you want"
 
 
-@app.route('/start')
+@app.route('/start', methods=["POST"])
 def handle_start():
-    pass
+    return json.dumps({"what": "hey"})
 
 
-@app.route('/move')
+@app.route('/move', methods=["POST"])
 def handle_move():
     pass
 
 
-if __name__ == 'main':
-    formatter = logging.Formatter("{%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.ERROR)
-    handler.setFormatter(formatter)
-
-    app.logger.addHandler(handler)
-
-    logging.getLogger('flask_cors').level = logging.INFO
-
+if __name__ == '__main__':
     app.run()
